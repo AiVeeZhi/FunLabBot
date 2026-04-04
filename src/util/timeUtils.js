25ae25815fb01b00dc592e5e551202e8e1b8a875
AiVeeZhi <87202547+AiVeeZhi@users.noreply.github.com>
2026-04-03 23:15:07 -0500

    update date format check (#3)
    * update date format check
    
    * update date format check
    
    ---------
    
    Co-authored-by: Ivan <ileong445@hotmail.com>

--- src/util/timeUtils.js
+++ src/util/timeUtils.js
@@ -7,25 +7,24 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
 
-function parseCSTDate(input) {
-    const formats = [
-        "M/D/YYYY[@]h:mmA",
-        "M/D/YYYY[@]hA",
-        "M/D[@]h:mmA",
-        "M/D[@]hA",
-        "M/D/YYYY[@]H:mm", // Matches military if am/pm is missing
-        "M/D[@]H"
+const formats = [
+        "M/D/YYYYh:mmA",
+        "M/D/YYYYhA",
+        "M/Dh:mmA",
+        "M/DhA",
+        "M/D/YYYYH:mm", // Matches military if am/pm is missing
+        "M/DH"
     ];
+
+function parseCSTDate(input) {
 
     const date = dayjs(input.toUpperCase(), formats).tz("America/Chicago", true);
     console.log(date);
     return date.utc().format("YYYY-MM-DDTHH:mm:ss[Z]");
 }
 
-const formatRegex = /^\d{1,2}\/\d{1,2}(\/\d{4})?@\d{1,2}(:\d{2})?([ap]m)?$/i;
-
 function isValidDateTimeInput(input) {
-    return formatRegex.test(input);
+    return dayjs(input, formats).isValid();
 }
 
 module.exports = {
